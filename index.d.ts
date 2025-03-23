bc1b837feb3dce11c408e0e93b52ba3d54efbfd2
MadBrozzeR <git.public.s@madbr.ru>
2025-03-23 16:08:41 +0300

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,33 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
-declare module 'madsocket' {
-  type Listeners = {
-    start?: (this: MadSocket) => void;
-    close?: (this: MadSocket) => void;
+type Listeners = {
+  start?: (this: MadSocket) => void;
+  close?: (this: MadSocket) => void;
 
-    error?: (this: MadSocket | Client, error: any) => void;
+  error?: (this: MadSocket | Client, error: any) => void;
 
-    connect?: (this: Client) => void;
-    disconnect?: (this: Client) => void;
-    message?: (this: Client, message: string) => void;
-  };
+  connect?: (this: Client) => void;
+  disconnect?: (this: Client) => void;
+  message?: (this: Client, message: string) => void;
+};
 
-  type Props = {
-    timeout?: number;
-    debug?: (type: string, data: Buffer) => void;
-  };
+type Props = {
+  timeout?: number;
+  debug?: (type: string, data: Buffer) => void;
+};
 
-  export class MadSocket {
-    constructor(listeners: Listeners, props: Props);
+export class MadSocket {
+  constructor(listeners?: Listeners, props?: Props);
 
-    listen(port: number, host?: string);
+  listen(port?: number, host?: string);
 
-    close();
+  close();
 
-    leach(request: IncomingMessage, response: ServerResponse<IncomingMessage>); // DEPRECATED
-    leech(request: IncomingMessage, response: ServerResponse<IncomingMessage>);
-  }
+  leach(request: IncomingMessage, response: ServerResponse<IncomingMessage>); // DEPRECATED
+  leech(request: IncomingMessage, response: ServerResponse<IncomingMessage>);
+}
 
-  export class Client {
-    send(message: string);
-    close(status?: number, reason?: string);
-  }
+export class Client {
+  send(message: string);
+  close(status?: number, reason?: string);
 }
